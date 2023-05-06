9ebae50f3f6d769df8827cbed45e2be652c8fb73
Shashi Shekhar <shekhars094@gmail.com>
2023-05-06 11:09:26 +0530

    design fix

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -96,7 +96,8 @@ const ShoppingCardItem = ({ id, name, price, image, quantity }) => {
         flexDirection: "column",
         justifyContent: "center",
         borderRadius: "5px",
-        boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.75)",
+        border: "1px solid black",
+        backgroundColor: "white",
         padding: "24px",
       }}
     >
